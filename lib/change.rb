93a0b5ea1c5ebfad31d75bfa8cc4362eadb42841
Amy Balmforth <amy.balmforth@gmail.com>
2019-12-24 13:15:44 +0000

    second test, return 1p for 1

--- lib/change.rb
+++ lib/change.rb
@@ -1,7 +1,10 @@
 class Change
 
   def make_change(num)
-    {}
+    if num == 0
+      return {}
+    end
+    return {:P=>1}
   end
 
 end
--- spec/change_spec.rb
+++ spec/change_spec.rb
@@ -1,9 +1,19 @@
 require "change"
 describe Change do
 
+  # 1 -> {:P=>1}
+  # 5 -> {:N=>1}
+  # 10 -> {:D=>1}
+  # 25 -> {:Q=>1}
+  # 50 -> {:H=>1}
+
   describe "zero or 1 coin" do
     it "take in zero, return empty hash" do
       expect(subject.make_change(0)).to eq ({})
+    end
+
+    it "take in 1, return {:P=>1}" do
+      expect(subject.make_change(1)).to eq ({:P=>1})
     end
   end
 
